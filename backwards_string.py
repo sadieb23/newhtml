5e3b84e77ae0d67de91f6d0c4375b67076e3b377
sadieb23 <sadiebalk3@gmail.com>
2023-09-02 11:09:36 -0400

    Initial Commit

---
+++ backwards_string.py
@@ -0,0 +1,10 @@
+#Gets a string input from the user
+user_input = input("Enter a string: ")
+
+#Reverses the string by slicing with a step of -1
+reversed_string = user_input[::-1]
+
+#Prints the reversed string
+print(reversed_string)
+
+#used chatgpt for help
